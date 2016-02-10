e9d1a9f7cf73908e8c349c39f518fb2312e85ff3
Znoraka <znoraka@gmail.com>
2016-02-10 17:28:47 +0100

    ajout plot

--- phylogeny-generator/src/main.cpp
+++ phylogeny-generator/src/main.cpp
@@ -6,9 +6,12 @@
 #include <fstream>
 #include <ctime>
 #include <Magick++.h>
+#include <map>
 extern "C" {
 #include <jpeglib.h>
 }
+
+#include "gnuplot_i.hpp"
 
 using namespace Magick;
 
@@ -26,9 +29,101 @@ void plotDctCoefficients(std::ostream &stream, std::string path);
 void plotDctCoefficients(std::ostream &stream, std::string path) {
   struct jpeg_decompress_struct info;
   struct jpeg_error_mgr err;
+  jvirt_barray_ptr *coeffs;
+
+  Gnuplot g1("lines");
+  
   info.err = jpeg_std_error(&err);     
-  // jpeg_create_decompress(&info);
-  // jpeg_read_coefficients(NULL);
+  jpeg_create_decompress(&info);
+
+  FILE *fp;
+  if((fp = fopen(path.c_str(), "rb")) == NULL) {
+    std::cout << "could not open image : " << path << std::endl;
+    return;
+  }
+  
+  jpeg_stdio_src(&info, fp);
+  (void) jpeg_read_header(&info, true);
+  coeffs = jpeg_read_coefficients(&info);
+
+
+  //parcours zigzag
+  int zigzag[64];
+  int n = 0;
+  for (int i = 0; i < 8 * 2; i++)
+    for (int j = (i < 8) ? 0 : i-8+1; j <= i && j < 8; j++)
+      zigzag[n++] = (i&1)? j*(8-1)+i : (i-j)*8+j;
+
+  for (int channel = 0; channel < 1; channel++) {
+    // for (int dctIndex = 0; dctIndex < 1; dctIndex++) {
+    for (int dctIndex = 0; dctIndex < 16; dctIndex++) {
+      std::map<int, int> histo;
+      for (int i = 0; i < info.comp_info->height_in_blocks; i++) {
+	for (int j = 0; j < info.comp_info->width_in_blocks; j++) {
+
+	  
+	  int n = info.mem->access_virt_barray((j_common_ptr)&info, coeffs[channel], i, 1, false)[0][j][zigzag[dctIndex]];
+
+	  auto s = histo.find(n);
+	  
+	  if(s != histo.end()) {
+	    s->second += 1;
+	  } else {
+	    histo.insert(std::pair<int, int>(n, 1));
+	  }
+	}
+      }
+
+      std::vector<int> x;
+      std::vector<int> y;
+      for(auto i : histo) {
+	x.push_back(i.first);
+	y.push_back(i.second);
+      }
+
+      // montage -geometry 640x480+0+0 {0..15}.png out.png
+      
+      g1.cmd("set xrange [-400:400]");
+      g1.cmd("set style data histogram");
+      g1.cmd("set style histogram clustered");
+      g1.cmd("set style fill solid border");
+      g1.cmd("set term png");
+      std::string s;
+      s += std::to_string(dctIndex) + ".png";
+
+      g1.set_style("boxes");
+      
+      g1.cmd("set output \"" + s + "\"");
+      g1.plot_xy(x, y, "");
+      g1.cmd("replot");
+      g1.reset_plot();
+    }
+  }
+  // for (int ci = 0; ci < 1; ci++)
+  //   {
+  //     JBLOCKARRAY buffer_one;
+  //     JCOEFPTR blockptr_one;
+  //     jpeg_component_info* compptr_one;
+  //     compptr_one = info.comp_info + ci;
+
+  //     for (int by = 0; by < 1; by++)
+  //     // for (int by = 0; by < compptr_one->height_in_blocks; by++)
+  //       {
+  // 	  buffer_one = (info.mem->access_virt_barray) ((j_common_ptr)&info, src_coef_arrays[ci], by, (JDIMENSION)1, FALSE);
+  // 	  for (int bx = 0; bx < 1; bx++)
+  // 	  // for (int bx = 0; bx < compptr_one->width_in_blocks; bx++)
+  //           {
+  // 	      blockptr_one = buffer_one[0][bx];
+  // 	      // QVector<int> tmp;
+  // 	      for (int bi = 0; bi < 64; bi++)
+  //               {
+  // 		  std::cout << blockptr_one[bi] << std::endl;
+  // 	      	  // tmp.append(blockptr_one[bi]);
+  //               }
+  // 	      // dct_coeff.push_back(tmp);
+  //           }
+  //       }
+  //   }  
 }
 
 void applyTransform(Image& image) {
@@ -142,6 +237,9 @@ void exportPhylogeny(std::ostream& stream, Node *root) {
 int main(int argc, char **argv){
   std::srand(std::time(NULL));
   InitializeMagick(*argv);
+
+  plotDctCoefficients(std::cout, argv[2]);
+
 
   Node *root = createPhylogeny(argv[1], argv[2], atoi(argv[3]));
   std::string treePath = std::string(argv[1]) + "tree.tex";
