86e2ad77d87a4e8a7edf435ef8c0d3c8af563dc3
noe <noe.lephilippe@gmail.com>
2016-06-02 23:40:26 +0200

    ajout résultats

--- tree-extractor/c++/CMakeFiles/tree-extractor.dir/cmake_clean.cmake
+++ tree-extractor/c++/CMakeFiles/tree-extractor.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/tree-extractor.dir/src/main.cpp.o"
   "bin/tree-extractor.pdb"
   "bin/tree-extractor"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/tree-extractor.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tree-extractor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
